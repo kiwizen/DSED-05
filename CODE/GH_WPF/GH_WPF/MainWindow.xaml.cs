90063f47c7178e7bcfbd35faa269f2a4657c720b
KEUM HONG LIM <kiwizen.code@hotmail.com>
2017-05-21 22:18:25 +1200

    Added some new image and some animation

--- CODE/GH_WPF/GH_WPF/MainWindow.xaml.cs
+++ CODE/GH_WPF/GH_WPF/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace GH_WPF
 {
@@ -20,9 +21,34 @@ namespace GH_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 50);
+            timer.Tick += new EventHandler(MoveImage);
+            timer.Start();
+            //new Task(() => AnimateObject(image1, 0.0)).RunSynchronously();
+        }
+
+
+        private void MoveImage(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
+            Canvas canvas = image1.Parent as Canvas;
+            long pos = Convert.ToInt64(image1.GetValue(Canvas.LeftProperty));
+            if ( pos >= canvas.Margin.Left + canvas.ActualWidth - image1.Width - canvas.Margin.Right)
+            {
+                timer.Stop();
+                return;
+            }
+            else 
+                Canvas.SetLeft(image1, pos + 10);
         }
     }
 }
